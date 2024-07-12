215737a4b2b3e2ca9c092b1afc89555a568d7113
Juanpibazan <juampivader1@gmail.com>
2024-07-12 02:31:47 -0400

    changed sql icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,7 +21,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-import sql from './sql.png';
+import sql from './sql-sin-fondo.png';
 import pythonIcon from './python-sin-fondo.png';
 
 import meta from "./company/meta.png";
