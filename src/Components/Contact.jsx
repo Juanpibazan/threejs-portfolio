f102dfc86e391ae938a8ffa658e89104da1d8d50
Juanpibazan <juampivader1@gmail.com>
2024-07-17 01:35:00 -0400

    Added form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -39,12 +39,53 @@ const Contact = () => {
         className='mt-12 flex flex-col gap-8'
         >
           <label
-          className='flex flex-col'>
+          className='flex flex-col gap-2'>
             <span className='text-white font-medium'>Your Name</span>
-            <input />
+            <input type='text' placeholder='John Doe' value={form.name}
+            onChange={(e)=>setForm((current)=>({...current,name:e.target.value}))}
+            className='border-white border-2 rounded-md p-2 outline-none font-medium'
+            />
           </label>
-
+          <label
+          className='flex flex-col gap-2'>
+            <span className='text-white font-medium'>Your E-Mail</span>
+            <input type='email' placeholder='john_doe@mail.com' value={form.email}
+            onChange={(e)=>setForm((current)=>({...current,email:e.target.value}))}
+            className='border-white border-2 rounded-md p-2 outline-none font-medium'
+            />
+          </label>
+          <label
+          className='flex flex-col gap-2'>
+            <span className='text-white font-medium'>Your Message</span>
+            <textarea placeholder='My message is...' value={form.message}
+            onChange={(e)=>setForm((current)=>({...current,message:e.target.value}))}
+            className='border-white border-2 rounded-md p-2 outline-none font-medium'
+            />
+          </label>
+          <button
+          type='submit'
+          className='bg-black text-white text-[20px] px-3 py-2
+          border-white border-2 rounded-lg shadow-md hover:bg-white hover:text-black active:bg-white active:text-black hover:font-bold'
+          >
+            {loading ? 'Sending...' : 'Send'}
+          </button>
         </form>
+      </motion.div>
+      <motion.div
+      initial={{
+        opacity:0
+      }}
+      animate={{
+        opacity:1
+      }}
+      transition={{
+        type:'spring',
+        delay:0.5,
+        duration:4
+      }}
+      
+      >
+        <h1 className='text-white'>Hi</h1>
       </motion.div>
     </div>
   )
